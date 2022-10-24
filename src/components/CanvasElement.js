56ea8fa3bd6e37346fa0710672b5b6355e35d19b
Tim-Poels <timpoels2000@gmail.com>
2022-10-24 10:56:14 +0200

    added orbitcontrols

--- src/components/CanvasElement.js
+++ src/components/CanvasElement.js
@@ -1,13 +1,34 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber"
+import React, { Suspense, useRef, useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
 import * as THREE from "three"
 import { useGLTF } from "@react-three/drei"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  useEffect(
+    () => {
+      const controls = new OrbitControls(camera, gl.domElement);
+
+      controls.minDistance = 2;
+      controls.maxDistance = 10;
+      return () => {
+        controls.dispose();
+      };
+    },
+    [camera, gl]
+  );
+  return null;
+};
 
 const CanvasElement = (props) => {
   return (
     <Canvas className="CanvasElement">
+      <CameraController />
+
       <ambientLight intensity={0.4} />
       <directionalLight color="white" position={[0, 0, 5]} />
+
       <Suspense fallback={null}>
         <Bed />
       </Suspense>
@@ -15,8 +36,15 @@ const CanvasElement = (props) => {
   );
 }
 
+const Controls = () => {
+  return (
+    <OrbitControls />
+  )
+}
+
 const Bed = () => {
   const { scene } = useGLTF('bed-test.glb')
+  scene.children[0].position.set(0, -1, 0);
   return <primitive object={scene} />
 }
 
