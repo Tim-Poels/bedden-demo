9898e6c222b044a1b3f4fc7f8de9a6059d96cc18
Tim-Poels <timpoels2000@gmail.com>
2022-10-28 11:47:16 +0200

    added 3 more textures for testing

--- src/components/steps/Textures.js
+++ src/components/steps/Textures.js
@@ -1,13 +1,31 @@
-import whiteAO from "../../assets/textures/AO/white.jpg"
-import whiteColor from "../../assets/textures/Color/white.jpg"
-import whiteNormal from "../../assets/textures/Normal/white.jpg"
-import whiteRoughness from "../../assets/textures/Roughness/white.jpg"
-import whiteImg from "../../assets/textures/Color/white.jpg"
+import whiteAO from "../../assets/textures/AO/white-test.jpg"
+import whiteColor from "../../assets/textures/Color/white-test.jpg"
+import whiteNormal from "../../assets/textures/Normal/white-test.jpg"
+import whiteRoughness from "../../assets/textures/Roughness/white-test.jpg"
+import whiteImg from "../../assets/textures/Color/white-test.jpg"
+
+import redAO from "../../assets/textures/AO/red-test.jpg"
+import redColor from "../../assets/textures/Color/red-test.jpg"
+import redNormal from "../../assets/textures/Normal/red-test.jpg"
+import redRoughness from "../../assets/textures/Roughness/red-test.jpg"
+import redImg from "../../assets/textures/Color/red-test.jpg"
+
+import orangeAO from "../../assets/textures/AO/orange-test.jpg"
+import orangeColor from "../../assets/textures/Color/orange-test.jpg"
+import orangeNormal from "../../assets/textures/Normal/orange-test.jpg"
+import orangeRoughness from "../../assets/textures/Roughness/orange-test.jpg"
+import orangeImg from "../../assets/textures/Color/orange-test.jpg"
+
+import yellowAO from "../../assets/textures/AO/yellow-test.jpg"
+import yellowColor from "../../assets/textures/Color/yellow-test.jpg"
+import yellowNormal from "../../assets/textures/Normal/yellow-test.jpg"
+import yellowRoughness from "../../assets/textures/Roughness/yellow-test.jpg"
+import yellowImg from "../../assets/textures/Color/yellow-test.jpg"
 
 const textures = [
   {
-    Id: "white",
-    Name: "white",
+    Id: "white-test",
+    Name: "white-test",
     Img: whiteImg, 
     AO: whiteAO,
     Color: whiteColor,
@@ -15,31 +33,31 @@ const textures = [
     Roughness: whiteRoughness,
   },
   {
-    Id: "red",
-    Name: "red",
-    Img: whiteImg, 
-    AO: whiteAO,
-    Color: whiteColor,
-    Normal: whiteNormal,
-    Roughness: whiteRoughness,
+    Id: "red-test",
+    Name: "red-test",
+    Img: redImg, 
+    AO: redAO,
+    Color: redColor,
+    Normal: redNormal,
+    Roughness: redRoughness,
   },
   {
-    Id: "cyan",
-    Name: "cyan",
-    Img: whiteImg, 
-    AO: whiteAO,
-    Color: whiteColor,
-    Normal: whiteNormal,
-    Roughness: whiteRoughness,
+    Id: "orange-test",
+    Name: "orange-test",
+    Img: orangeImg, 
+    AO: orangeAO,
+    Color: orangeColor,
+    Normal: orangeNormal,
+    Roughness: orangeRoughness,
   },
   {
-    Id: "lime",
-    Name: "lime",
-    Img: whiteImg, 
-    AO: whiteAO,
-    Color: whiteColor,
-    Normal: whiteNormal,
-    Roughness: whiteRoughness,
+    Id: "yellow-test",
+    Name: "yellow-test",
+    Img: yellowImg, 
+    AO: yellowAO,
+    Color: yellowColor,
+    Normal: yellowNormal,
+    Roughness: yellowRoughness,
   },
 ]
 
